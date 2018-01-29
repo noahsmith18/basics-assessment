f7c61191e120e0f5ec2389cacc0d35ce895c7f8a
Noah Smith <noahsmith@Interapts-MacBook-Pro-34.local>
2018-01-29 14:49:01 -0500

    finished assessment

---
+++ Basics assessment/main.swift
@@ -0,0 +1,35 @@
+//
+//  main.swift
+//  Basics assessment
+//
+//  Created by Noah Smith on 1/29/18.
+//  Copyright © 2018 Noah Smith. All rights reserved.
+//
+
+import Foundation
+
+//1. Fix this block of code!
+var number1: Float = 2
+var number2: Float = 2
+var sum = number1 + number2
+
+//2. Create a constant called answer. Answer should be equal to a plus b minus c modulo 2
+var a = 2
+var b = 3
+var c = 4
+var string: Int = a + b - c 
+
+//3. Create two variables called firstName and lastName. Create a third variable called fullname that is your first name and last name combined using string interpolation.
+var firstName = "Noah"
+var lastName = "Smith"
+var fullName = firstName + " " + lastName
+
+//4. Using a comment list 2 rules for creating variables.
+//Rule 1. You can't add or subtract variables if they're not the same type of variable
+//Rule 2.
+
+
+//5. Using a for loop and an if statment print out every even number from 1 to 100
+for i in 1...100 {
+    print(i)
+}
